bf0d4b96a8046ee3f7e19c54221d5b8797b8b31c
Namindu <namindu@live.com>
2024-07-06 12:46:01 +0530

    cccs

--- flask_app.py
+++ flask_app.py
@@ -39,4 +39,4 @@ def ask():
         return jsonify({'error': 'Invalid content type, expected application/json'}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
